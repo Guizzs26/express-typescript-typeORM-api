b03a23834a564e6c7c9b43c48c71ecb8f694526d
Guilherme <guiteixera1926@gmail.com>
2023-12-16 18:53:06 -0400

    Adding libs cors and express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,19 @@
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
 
-app.liste(3000, () => {
-  console.log('Servido inicializado, teste');
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Olá dev' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servido inicializado na porta ${process.env.PORT}`);
 });
