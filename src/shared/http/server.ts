498836bbdfef10d6235cf267c90c49efebc89467
Guilherme <guiteixera1926@gmail.com>
2023-12-16 20:48:21 -0400

    Install and config Swagger

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,14 +1,17 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 import { router } from './routes';
 import { AppError } from '@shared/errors/AppError';
+import swaggerFile from '../../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
